ad13a9bb2740ad22421d5481cf4c9a3403f199b2
Mihail Sinelnikov <79084786093@mail.ru>
2018-05-20 03:36:00 +0400

    http connecting with google

---
+++ app/src/main/java/com/example/mihail/taxitest/GeoHelper.kt
@@ -0,0 +1,54 @@
+package com.example.mihail.taxitest
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+import org.json.JSONObject
+
+
+class GeoHelper(val context: Context) {
+
+    val tag = "ApiCoder"
+
+    val staticUrl = "https://maps.googleapis.com/maps/api/geocode/json?language=ru"
+    val apiKey = "&key=${ApiConfig.TOKEN_API_GOOGLE_GEOCODING}"
+    var URL = staticUrl
+
+    fun addParamLglt(lt: Double, lg: Double): String {
+        var url = URL
+        url += "&latlng=$lt,$lg"
+        return url
+    }
+
+    fun addParam(param: String, value: String): String {
+        var url = URL
+        url += "&$param=$value"
+        URL = url
+        return url
+    }
+
+    fun connect(urls: String, urlX: (String) -> Unit) {
+        val url = urls + apiKey
+        val queue = Volley.newRequestQueue(context)
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+//                    textView.text = "Response is: ${response.substring(0, 500)}"
+                    urlX(response)
+                },
+                Response.ErrorListener { })
+        queue.add(stringRequest)
+    }
+
+    fun getStreetFromJson(jsonObject: JSONObject):String{
+        val home = ((jsonObject.getJSONArray("results")[0] as JSONObject).getJSONArray("address_components")[0] as JSONObject).get("long_name")
+        val street = ((jsonObject.getJSONArray("results")[0] as JSONObject).getJSONArray("address_components")[1] as JSONObject).get("long_name")
+        val city = ((jsonObject.getJSONArray("results")[0] as JSONObject).getJSONArray("address_components")[2] as JSONObject).get("long_name")
+        return "$street,$home,$city"
+    }
+
+
+}
--- app/src/main/java/com/example/mihail/taxitest/MainActivity.kt
+++ app/src/main/java/com/example/mihail/taxitest/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mihail.taxitest
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
@@ -12,15 +11,15 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.common.api.PendingResults
-import com.google.android.gms.location.places.PlaceReport
-//import com.google.android.gms.location.places.PlaceDetectionApi
-//import com.google.android.gms.location.places.Places
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraListener
+import com.yandex.mapkit.map.CameraUpdateSource
+import com.yandex.mapkit.map.Map
+import org.json.JSONObject
 
 
 class MainActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedListener {
@@ -28,12 +27,14 @@ class MainActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedListe
         print("fail")
     }
 
+    val tag = "MainActivity"
+
     private lateinit var mapView: MapView
     private lateinit var searchBtn: RelativeLayout
     private lateinit var settingsBtn: RelativeLayout
     private lateinit var homeBtn: RelativeLayout
     private lateinit var labelTxt: TextView
-
+    private val geoHelper = GeoHelper(this)
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,12 +48,14 @@ class MainActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedListe
         settingsBtn = findViewById(R.id.settings)
         labelTxt = findViewById(R.id.label)
 
+
+
         if (isGeoDisabled()){
             startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
         }
 
         mapView.map.move(
-                CameraPosition(Point( 54.300, 48.3300), 12.0f, 0.0f, 0.0f),
+                CameraPosition(Point( 54.300, 48.3300), 16.0f, 0.0f, 0.0f),
                 Animation(Animation.Type.SMOOTH, 0f),
                 null)
         searchBtn.setOnClickListener {
@@ -60,10 +63,19 @@ class MainActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedListe
             //            labelTxt.text = "${}"
 //            labelTxt.text = "${mapView.map.cameraPosition.target.latitude}/${mapView.map.cameraPosition.target.longitude}"
         }
-        val coder = ApiGeoCoder()
-        coder.addParam("latlng","${54.300},${48.3300}")
-        coder.connect()
 
+//        geoHelper.addParam("latlng","${54.300},${48.3300}")
+//        geoHelper.connect(geoHelper.addParamLglt(54.300.toString(), 48.300.toString())){
+//            val json = JSONObject(it)
+//           labelTxt.text = geoHelper.getStreetFromJson(json)
+//        }
+        mapView.map.addCameraListener(object :CameraListener{
+            override fun onCameraPositionChanged(p0: Map?, p1: CameraPosition?, p2: CameraUpdateSource?, p3: Boolean) {
+                showAddress(p1!!.target.latitude,p1.target.longitude)
+            }
+
+        })
+//        showAddress(54.300, 48.3300)
 
 //
     }
@@ -87,4 +99,12 @@ class MainActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedListe
         val mIsNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
         return !mIsGPSEnabled && !mIsNetworkEnabled
     }
+
+    fun showAddress(lt: Double, lg: Double){
+        geoHelper.connect(geoHelper.addParamLglt(lt, lg)){
+            val json = JSONObject(it)
+            labelTxt.text = geoHelper.getStreetFromJson(json)
+        }
+    }
+
 }
